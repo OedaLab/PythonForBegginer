f3b9158fbf4321788b8dba3a4cc36a582dc3658f
crotsu <crotsu@gmail.com>
2015-09-07 18:06:36 +0900

    No4を修正

--- No4.py
+++ No4.py
@@ -1,4 +1,4 @@
-# 問題5
+# 問題4
 # ランダムに0から9までの値を10個出力しなさい．
 
 import random
@@ -6,4 +6,7 @@ import random
 val = []
 
 for i in range(10):
-    
+    r = random.randint(0,9)
+    val.append(r)
+
+print(val)
